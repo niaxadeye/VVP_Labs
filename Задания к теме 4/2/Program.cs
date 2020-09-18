6a75ad434cb39b220ac2a0a1d10183983b46e36a
Эрик Фаттахов <niaxadeye@outlook.com>
2020-09-18 20:42:49 +0300

    Добавил решение 2 задания

---
+++ Задания к теме 4/2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static public double calc_L(int d)
+        {
+            double pi = 3.14, L;
+            L = pi * d;
+            return L;
+
+        }
+        static void Main(string[] args)
+        {
+            int d;
+            double L;
+            Console.Write("Введите диаметр окружности: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            L = calc_L(d);
+            Console.WriteLine($"Для окружности с диаметром {d}, ддлина будет равна {L}");
+        }
+    }
+}
