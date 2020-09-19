9b9833b25a800177b3d596ea12f8a5f0eeb646e6
Эрик Фаттахов <niaxadeye@outlook.com>
2020-09-19 11:57:03 +0300

    Добавил решение задачи №3

---
+++ Задания к теме 4/3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static decimal calc_SRAR(decimal a, decimal b)
+        {
+            decimal SRAR;
+            SRAR = (a + b)/ 2;
+            return (decimal)SRAR;
+        }
+        static void Main(string[] args)
+        {
+            decimal a, b, SRAR;
+            a = Convert.ToDecimal(Console.ReadLine());
+            b = Convert.ToDecimal(Console.ReadLine());
+            SRAR = calc_SRAR(a, b);
+            Console.WriteLine(SRAR);
+        }
+    }
+}
